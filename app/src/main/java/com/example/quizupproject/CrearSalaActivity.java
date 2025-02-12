1b57b23f4e8a63b7792d464acf99f15d308a0891
MarioGS <mgs0077@alu.medac.es>
2025-02-12 10:19:15 +0100

    crear sala activity arreglado

--- app/src/main/java/com/example/quizupproject/CrearSalaActivity.java
+++ app/src/main/java/com/example/quizupproject/CrearSalaActivity.java
@@ -1,8 +1,6 @@
 package com.example.quizupproject;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -13,30 +11,27 @@ import com.example.quizupproject.client.Cliente;
 
 public class CrearSalaActivity extends AppCompatActivity {
 
-    private EditText nombreSalaEditText, contrasenaEditText;
+    private EditText nombreSalaEditText;
     private ImageView botonCrearSala;
     private Cliente cliente;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.crear_sala); // El layout de la pantalla que compartiste
+        setContentView(R.layout.crear_sala);
 
-        nombreSalaEditText = findViewById(R.id.introducir_correo);
-        contrasenaEditText = findViewById(R.id.editText2);
-        botonCrearSala = findViewById(R.id.boton_crear_sesion);  // Asegúrate de que el ID coincide con el de tu layout
+        nombreSalaEditText = findViewById(R.id.codigodesala);
+        botonCrearSala = findViewById(R.id.boton_crear_sesion);
 
         cliente = new Cliente();  // Cliente será el que se encargue de la comunicación
 
         botonCrearSala.setOnClickListener(v -> {
-            String nombreSala = nombreSalaEditText.getText().toString().trim();
-            String contrasenaSala = contrasenaEditText.getText().toString().trim();
+            String codigoSala = nombreSalaEditText.getText().toString().trim();
 
-            if (!nombreSala.isEmpty() && !contrasenaSala.isEmpty()) {
-                cliente.conectarServidor(contrasenaSala);
+            if (!codigoSala.isEmpty()) {
+                cliente.conectarServidor(codigoSala);
             } else {
-                Toast.makeText(CrearSalaActivity.this, "Por favor, ingresa los datos correctamente", Toast.LENGTH_SHORT).show();
+                Toast.makeText(CrearSalaActivity.this, "Por favor, ingresa el código de la sala", Toast.LENGTH_SHORT).show();
             }
         });
     }
